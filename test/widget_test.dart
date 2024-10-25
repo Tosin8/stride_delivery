5f7d0d07362818f6678992218e5fc33cfec3f1c9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-10-25 21:27:26 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:stride_delivery/app.dart';
 
 import 'package:stride_delivery/main.dart';
 

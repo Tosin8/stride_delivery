6a1dc51945f1259ddfec0d0d8a6909e0f47cf1b8
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-01 23:18:19 +0100

    Updating

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:get/get.dart';
+import 'package:stride_delivery/bindings/details_binding.dart';
 import 'package:stride_delivery/screens/deliveries.dart';
 import 'package:stride_delivery/screens/navigation_menu.dart';
 
@@ -16,9 +17,16 @@ class AppRoutes {
 
         GetPage(name: BRoutes.signIn, page: () => const LoginScreen()), 
         
-       GetPage(name: '/navigation', page: () => NavigationMenu()),
-        GetPage(name: '/deliveryDetails', page: () => const DeliveryDetails(delivery: null,)),
-        GetPage(name: '/tracking', page: () => const TrackingScreen(deliveryAddress: '', customerName: '')),      
+        GetPage(name: '/navigation', page: () => const NavigationMenu()),
+        GetPage(
+          name: '/deliveryDetails',
+          page: () =>  DeliveryDetails(delivery: Get.arguments as Delivery,),
+          binding: DeliveryDetailsBinding(), // Use the binding here
+        ),
+        GetPage(
+          name: '/tracking',
+          page: () => const TrackingScreen(deliveryAddress: '', customerName: ''),
+        ),
         
         
       
